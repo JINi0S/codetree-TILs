ea8befb95f38d82d2b2092475eb427e99709fb56
LeeJinHee <wlsgml8847@kakao.com>
2024-10-09 16:47:08 +0900

    Title: 회전하는 빙하, Time: 594ms, Memory: 30MB, Status: Passed - CodeTree

--- 241009/회전하는 빙하/rotating-glacier.py
+++ 241009/회전하는 빙하/rotating-glacier.py
@@ -1,4 +1,4 @@
-# 14:35
+# 14:35 ~ 16:28
 # n: 회전 가능 레벨, 주어진 격자의 크기: 2의 n승 * 2의 n승
 # q: 회전 횟수
 n, q = map(int, input().split())
@@ -10,20 +10,22 @@ for i in range(N):
 
 
 rotateLevels = list(map(int, input().split()))
-print("LEVELS", rotateLevels)
+# print("LEVELS", rotateLevels)
 
 def printBoard():
     for i in range(N):
         print(board[i])
+
+
 ### 함수 구현부
 def rotate(mapWidth, oneBoardWidth): # 4, 2 => 레벨2
     for i in range(0, N, mapWidth):
         for j in range(0, N, mapWidth):
-            print("StartPoint", i, j)
+            # print("StartPoint", i, j)
             # 좌하, 우하, 우상, 좌상(저장값불러오기) 순으로 이동
             # 좌상 저장
             leto = [x[j:j+oneBoardWidth] for x in board[i:i+oneBoardWidth]]
-            print(leto)
+            # print(leto)
             
             #좌하 > 좌상
             for ni in range(i, i+oneBoardWidth):
@@ -31,18 +33,90 @@ def rotate(mapWidth, oneBoardWidth): # 4, 2 => 레벨2
                     board[ni][nj] = board[ni+oneBoardWidth][nj]
             
             #우하 > 좌하
-            for ni in range(i, i+oneBoardWidth):
-                for nj in range(j+oneBoardWidth, j+oneBoardWidth+oneBoardWidth):
-                    print("PP",ni, nj)
+            for ni in range(i+oneBoardWidth, i+oneBoardWidth+oneBoardWidth):
+                for nj in range(j, j+oneBoardWidth):
                     board[ni][nj] = board[ni][nj+oneBoardWidth]
 
+            #우상 > 우하
+            for ni in range(i+oneBoardWidth, i+oneBoardWidth+oneBoardWidth):
+                for nj in range(j+oneBoardWidth, j+oneBoardWidth+oneBoardWidth):
+                    board[ni][nj] = board[ni-oneBoardWidth][nj]
+            
+            #백업 > 우상
+            for ni in range(i, i+oneBoardWidth):
+                for nj in range(j+oneBoardWidth, j+oneBoardWidth+oneBoardWidth):
+                    board[ni][nj] = leto[ni%oneBoardWidth][nj%oneBoardWidth]
 
-    printBoard()
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def inRange(x):
+    if 0<=x<N:
+        return True
+    else:
+        return False
+
+def melting():
+    global dx, dy, board
+
+    newBoard = [x[:] for x in board]
+    # print("BEFORE====", board)
+
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] <= 0: continue
+            cnt = 0
+            for d in range(4):
+                nx, ny = i + dx[d], j + dy[d]
+                
+                if inRange(nx) and inRange(ny) and board[nx][ny] >= 1:
+                    cnt += 1
+            if cnt < 3:
+                newBoard[i][j] -= 1
+
+    board = [x[:] for x in newBoard]
+    # print("AFTER====", board)
+    # print("===========")
+
+
+def printRes():
+    global dx, dy
+
+    sumV = 0
+
+    stk = []
+    visited = [[False for _ in range(N)] for i in range(N)]
+    maxSize = 0
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] <= 0: continue # ????
+            if visited[i][j]: continue
+            stk.append((i, j))
+            visited[i][j] = True
+            size = 1
+            sumV += board[i][j]
+            while stk:
+                s = stk.pop()
+                for d in range(4):
+                    nx, ny = s[0] + dx[d], s[1] + dy[d]
+                    if inRange(nx) and inRange(ny) and board[nx][ny] >= 1 and not visited[nx][ny]:
+                        size += 1
+                        visited[nx][ny] = True
+                        sumV += board[nx][ny]
+                        stk.append((nx, ny))
+            maxSize = max(maxSize, size)
+
+    print(sumV)
+    print(maxSize)
 
 
 ### 수행부
 for level in rotateLevels:
-    rotateChoiceWidth = pow(2, level)
-    rotateOneBoardWidth = pow(2, level-1)
-    print(level, rotateChoiceWidth, rotateOneBoardWidth)
-    rotate(rotateChoiceWidth, rotateOneBoardWidth)
+    if level != 0: 
+        rotateChoiceWidth = pow(2, level)
+        rotateOneBoardWidth = pow(2, level-1)
+        # print(level, rotateChoiceWidth, rotateOneBoardWidth)
+        rotate(rotateChoiceWidth, rotateOneBoardWidth)
+    melting()
+
+printRes()
