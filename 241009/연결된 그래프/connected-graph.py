07a392b6b816d4db7486b1401e826da74f60dc18
LeeJinHee <wlsgml8847@kakao.com>
2024-10-09 23:31:23 +0900

    Title: 연결된 그래프, Time: 5079ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 241009/연결된 그래프/connected-graph.py
+++ 241009/연결된 그래프/connected-graph.py
@@ -8,20 +8,33 @@ for _ in range(m):
 
 nodes = set()
 visited = [False for i in range(n+1)]
-stk = [dic.get(1, [])]
+stk = [1]
 
 for d in dic.get(1, []):   
     nodes.add(d)
 
 while stk:
-    slist = stk.pop()
-    for s in slist:
-        nslist = dic.get(s, [])
-        for ns in nslist:
-            if visited[ns] and ns != 1: continue
-            visited[ns] = True
-            nodes.add(ns)
-            stk.append([ns])
+    current= stk.pop()
+    for s in dic.get(current, []):
+        if visited[s] and s != 1: continue
+        visited[s] = True
+        nodes.add(s)
+        stk.append(s)
 
 nodes.discard(1)
 print(len(nodes))
+
+# visited = [False for _ in range(n+1)]
+# stk = [1]  # 시작 노드 1을 스택에 넣음
+# visited[1] = True  # 시작 노드 1은 방문 처리
+# nodes = set()
+
+# while stk:
+#     current = stk.pop()  # 현재 노드 탐색
+#     for neighbor in dic.get(current, []):  # 인접 노드 탐색
+#         if not visited[neighbor]:  # 방문하지 않은 노드만
+#             visited[neighbor] = True  # 방문 처리
+#             nodes.add(neighbor)  # 노드 집합에 추가
+#             stk.append(neighbor)  # 스택에 추가
+
+# print(len(nodes))
